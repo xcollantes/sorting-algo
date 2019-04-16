46de6cbeeb995b5244bd7b0aa26a5d299b38231b
Xavier Collantes <xcollantes@google.com>
2019-04-16 16:12:31 -0700

    Add .h file

--- Main.cc
+++ Main.cc
@@ -8,18 +8,18 @@ using namespace std;
 int main(){
   cout << "Main Start" << endl;
   
-  Node *head = new Node();
-  Node *second = new Node();
+  Node *temp = new Node();
+
   cout << "Assigning Nodes." << endl;
-  head->data = 100;
-  head->next = NULL;
+  temp->data = 1000;
+  temp->next = NULL;
 
   cout << "Printing Nodes." << endl;
-  cout << head->data << endl;
-  cout << head->next << endl;
+  cout << temp->data << endl;
+  cout << temp->next << endl;
 
   cout << "Printing Node Address." << endl;
-  cout << head->data << endl;
-  cout << head->next << endl;
+  cout << temp->data << endl;
+  cout << temp->next << endl;
   return 0;
 }
